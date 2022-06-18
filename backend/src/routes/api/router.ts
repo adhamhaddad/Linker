2896e233ec5b2c7bfc526f8f147c8f0d88e99377
adhamhaddad <adham.ashraf.haddad@gmail.com>
2022-06-18 15:09:04 +0200

    some changes

--- backend/src/routes/api/router.ts
+++ backend/src/routes/api/router.ts
@@ -5,44 +5,20 @@ import path from "path";
 // Express Router
 const router = Router();
 
-// Requests
-router.get("/", (req: Request, res: Response) => {
-    res.status(200).json({
-        message: "Connecting Done!"
-    })
-})
-router.get("/home", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../../public/index"))
-})
-router.get("/profile", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../../public/profile"))
-})
-router.get("/profile_user", (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../../public/profile-port"))
-})
-router.get("/notifications", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../../public/home"))
-})
-router.get("/messages", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../../public/home"))
-})
-
+// Main Requests
+router.get("/", (_req: Request, res: Response) => res.status(200).json({message: "Connecting Done!"}))
+router.get("/home", (_req: Request, res: Response) => res.sendFile(path.join(__dirname, "../../../public/index")))
+router.get("/profile", (_req: Request, res: Response) => res.sendFile(path.join(__dirname, "../../../public/profile")))
+router.get("/profile_user", (_req: Request, res: Response) => res.sendFile(path.join(__dirname, "../../../public/profile-port")))
+router.get("/notifications", (_req: Request, res: Response) => res.sendFile(path.join(__dirname, "../../../public/home")))
+router.get("/messages", (_req: Request, res: Response) => res.sendFile(path.join(__dirname, "../../../public/home")))
 
 // Register Requests
-router.get("/register", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../../public/register"))
-})
-router.post("/register", register, (req: Request, res: Response) => {
-    res.redirect('/profile_user');
-})
-
+router.get("/register", (_req: Request, res: Response) => res.sendFile(path.join(__dirname, "../../../public/register")))
+router.post("/register", register, (_req: Request, res: Response) => res.redirect('/profile_user'))
 
 // Login Requests
-router.get("/login", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../../public/login"))
-})
-router.post("/login", login, (req: Request, res: Response) => {
-    res.redirect('/profile_user');
-});
+router.get("/login", (_req: Request, res: Response) => res.sendFile(path.join(__dirname, "../../../public/login")))
+router.post("/login", login, (_req: Request, res: Response) => res.redirect('/profile_user'));
 
 export default router;
--- backend/src/routes/handler.ts
+++ backend/src/routes/handler.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 import router from './api/router';
 import logger from '../middlewares/logger';
 
+// Express Router
 const handler = Router();
 
+// Express Requests Handler
 handler.use('/api', logger, router);
 
 export default handler;
