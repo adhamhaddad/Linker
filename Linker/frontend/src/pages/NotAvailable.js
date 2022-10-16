bab17246df322072b21ae25e4d58ed9ea321cf48
adhamhaddad <adhamhaddad.dev@gmail.com>
2022-10-16 04:45:26 +0200

    added spinner

--- Linker/frontend/src/pages/NotAvailable.js
+++ Linker/frontend/src/pages/NotAvailable.js
@@ -5,8 +5,12 @@ const UnderDevelopment = () => {
   return (
     <div className={classes['content']}>
       <h1>This page is under development.</h1>
+      <div className={classes['content-animation']}>
+        <i className={`fa-solid fa-cog ${classes['icon1']}`}></i>
+        <i className={`fa-solid fa-cog ${classes['icon2']}`}></i>
+      </div>
       <div className={classes['image-content']}></div>
     </div>
   );
 };
-export default UnderDevelopment
+export default UnderDevelopment;
