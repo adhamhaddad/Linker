60845611260b70c0eca3b52f343fe1b9ac62c654
adhamhaddad <adhamhaddad.dev@gmail.com>
2022-12-04 21:29:50 +0200

    added new table theme

---
+++ backend/migrations/sqls/20221204022828-themes-table-up.sql
@@ -0,0 +1,6 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS themes (
+    user_id uuid NOT NULL,
+    profile_cover VARCHAR(50),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
